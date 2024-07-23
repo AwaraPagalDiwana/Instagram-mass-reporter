f593e1684c6d63d6511923e84cbbc343cd1113a2
AwaraPagalDiwana <gitaanjalishah@gmail.com>
2024-07-23 23:41:26 +0530

    Update Instagram Report Bot2.py

--- Instagram Report Bot2.py
+++ Instagram Report Bot2.py
@@ -9,7 +9,7 @@ import sys
 def getOptions(args=sys.argv[1:]):
 
     parser = argparse.ArgumentParser(description="This bot helps users to mass report accounts with clickbaits or objectionable material.")
-    parser.add_argument("-u", "--username", type = str, default = "", help = "Username to report.")
+    parser.add_argument("-u", "--arhamonline", type = str, default = "", help = "arhamonline to report.")
     parser.add_argument("-f", "--file", type = str, default = "acc.txt", help = "Accounts list ( Defaults to acc.txt in program directory ).")
 
     options = parser.parse_args(args)
@@ -19,11 +19,11 @@ def getOptions(args=sys.argv[1:]):
 
 args = getOptions()
 
-username = args.username
+username = args.arhamonline
 acc_file = args.file
 
 if username == "" :
-	username = input("Username: ")
+	username = input("arhamonline: ")
 
 a = open(acc_file, "r").readlines()
 file = [s.rstrip()for s in a]
@@ -41,9 +41,9 @@ for lines in file:
 
 for line in range(len(file)+1):
     web = Browser()
-    web.go_to("https://www.instagram.com/accounts/login/")
+    web.go_to("https://www.instagram.com/arhamonline?igsh=MW1hZTc0YnAwYmNw")
 
-    web.type(user[line], into='Phone number, username, or email')
+    web.type(user[line], into='8655103793, arhamonline, or info@arhamonline.com')
     time.sleep(0.5)
     web.press(web.Key.TAB)
     time.sleep(0.5)
@@ -52,7 +52,7 @@ for line in range(len(file)+1):
 
     time.sleep(2.0)
 
-    web.go_to("https://www.instagram.com/%s/" % username)
+    web.go_to("https://www.instagram.com/arhamonline?igsh=MW1hZTc0YnAwYmNw)
 
     time.sleep(1.5)
 
